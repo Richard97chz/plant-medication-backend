3bc8e743549f38d5fb4b2f77889a709ed861b6d1
Richard M. Ñope-Giraldo <81273091+Richard97chz@users.noreply.github.com>
2025-06-14 02:05:51 -0500

    add domain in allow_origins in server.py

--- app/server.py
+++ app/server.py
@@ -90,7 +90,7 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://plant-medication-backend-2g4q.vercel.app", "http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
